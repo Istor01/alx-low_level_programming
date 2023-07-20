17e2a22dfa34637294657db237eaa90ec1c42729
Istor01 <vxaba3435@gmail.com>
2023-07-20 08:58:11 -0700

    0 > 972?

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,16 +10,16 @@
 
 int largest_number(int a, int b, int c)
 {
-		int largest;
+	int largest;
 
 	if (a >= b && b >= c)
 	{
 		largest = a;
 	}
-		else if (b >= a && b >= c)
-		{
-			largest = b;
-		}
+	else if (b >= a && b >= c)
+	{
+		largest = b;
+	}
 	else
 	{
 		largest = c;

18c9cd5f213ff9ef95c3c09fb40a7652c909bc4c
Istor01 <vxaba3435@gmail.com>
2023-08-04 18:08:56 -0700

    A library is not a luxury but one of the necessities of life

---
+++ 0x09-static_libraries/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * _strchr - Search a specified character and return pointer to the first occurrence of that character
+ * @s: input
+ * @c: input
+ * Return: Always 0 (Success)
+ */
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	for (; s[i] >= '\0'; i++)
+	{
+		if (s[i] == c)
+			return (&s[i]);
+	}
+	return (0);
+}
